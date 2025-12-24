b06f8f2c964a9888249889f5c0fd56bca1bf13c3
Aisyah Nur Fadilah <aisyahnfadilah73@gmail.com>
2025-12-24 21:39:55 +0700

    feat: add db config file

---
+++ config/db.php
@@ -0,0 +1,13 @@
+<?php
+    $host = "localhost";
+    $user = "root";
+    $pass = "";
+    $db   = "onelib_db";
+    $port = 8080;
+
+    $conn = mysqli_connect($host, $user, $pass, $db, $port);
+
+    if (!$conn) {
+        die("Koneksi gagal: " . mysqli_connect_error());
+}
+?>
